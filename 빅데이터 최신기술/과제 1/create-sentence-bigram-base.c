805641729de9a75ea97b7418ca15def63c41c9ae
Cha_Y_S <vaite714@gmail.com>
2021-03-28 03:44:51 +0900

    빈도 높은 unigram 랜덤 추출 구현

--- 빅데이터 최신기술/과제 1/create-sentence-bigram-base.c
+++ 빅데이터 최신기술/과제 1/create-sentence-bigram-base.c
@@ -22,6 +22,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+
+int Cnt[94][94][94][94];	// valid for <A1~FE, A1~FE>
+int CntBlank[94][94];	// freq -- syl+blank
+int CntBlankStart[94][94];	// freq -- blank+syl
 
 typedef struct _UnigramIndex{
     int i, j;
@@ -30,12 +35,15 @@ typedef struct _UnigramIndex{
 
 int compare(const void *a, const void *b);
 void swap(UnigramIndex *a, UnigramIndex *b);
-void swapIndex(UnigramIndex *uniArr, int hIndex, int lIndex, int cnt, int arrSize);
+void changeMinFreq(UnigramIndex *uniArr, int hIndex, int lIndex, int cnt, int arrSize);
 void unigramMaxFreq(char *fname, UnigramIndex *uniArr, int arrSize);
 void printArr(UnigramIndex *arr, int size);
+long freq_count_bigram(char *fname);
 
 int main(int argc, char* argv[]){
     int n, freq;
+	int uniHigh, uniLow;
+	char sentence[BUFSIZ];
 	UnigramIndex *unigram;
 
     if (argc < 3) { puts("Invalid command"); puts("Command: $create-sentence-bigram-base -n corpus.txt"); return 0; }
@@ -53,7 +61,57 @@ int main(int argc, char* argv[]){
 	printArr(unigram, freq);
 	printf("-------------------------------------------------\n");
 
+	srand(time(NULL));
+	int random = rand() % freq;
+	printf("choose one random unigram from list: %c%c\n\n", uniHigh=unigram[random].i+0xA1, uniLow=unigram[random].j+0xA1);
+
+	// TODO
+	// 	2. 선택된 음절 bigram의 빈도 가장 높은거 3개 중 하나 선택
+	//	3. 선택된 
+	// 
+
 	return 0;
+}
+
+long freq_count_bigram(char *fname){
+	int i, len;
+	int i1, i2, j1, j2;
+	char line[BUFSIZ];
+	long n=0;
+	FILE *fp;
+
+	fp = fopen(fname, "r");
+
+	while (!feof(fp)) {
+		fgets(line, BUFSIZ-1, fp);
+		len = strlen(line) - 1;
+
+		for (i=0; len-i > 3; ) {
+			if ((line[i+0] & 0x80) == 0) {
+				if (line[i+0] == ' ') CntBlankStart[i1][i2]++;	// blank + '음절'
+				i++;
+				continue;	// ASCII char
+			} else if ((line[i+2] & 0x80) == 0) {
+				if (line[i+2] == ' ') CntBlank[i1][i2]++;	// '음절' + blank
+				i += 3;
+				continue;	// ASCII char
+			} else {
+				i1 = (line[i+0] & 0x00FF) - 0x00A1;
+				i2 = (line[i+1] & 0x00FF) - 0x00A1;
+				j1 = (line[i+2] & 0x00FF) - 0x00A1;
+				j2 = (line[i+3] & 0x00FF) - 0x00A1;
+				if (i1 < 0 || i2 < 0 || j1 < 0 || j2 < 0) {
+					;	// non-KS C 5601 -- cp949 syllables
+				} else {
+					Cnt[i1][i2][j1][j2]++;
+					n++;
+				}
+				i += 4;
+			}
+		}
+	}
+	fclose(fp);
+	return n;
 }
 
 int compare(const void *a, const void *b){
@@ -68,13 +126,7 @@ int compare(const void *a, const void *b){
 		return 0;
 }
 
-void swap(UnigramIndex *a, UnigramIndex *b){
-	UnigramIndex temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-void swapIndex(UnigramIndex *uniArr, int hIndex, int lIndex, int cnt, int arrSize){
+void changeMinFreq(UnigramIndex *uniArr, int hIndex, int lIndex, int cnt, int arrSize){
 	UnigramIndex temp;
 	temp.i = hIndex;
 	temp.j = lIndex;
@@ -86,16 +138,15 @@ void swapIndex(UnigramIndex *uniArr, int hIndex, int lIndex, int cnt, int arrSiz
 
 void unigramMaxFreq(char *fname, UnigramIndex *uniArr, int arrSize){
 	int i, len;
-	char line[BUFSIZ];
 	int uniCnt[94][94];
 	int hIndex, lIndex;
-	int hByte=0xB0-0xA1, lByte=0;
 	int minCnt = 0;	// in UnigramIndex Array
 	FILE *fp;
 	
 	fp = fopen(fname, "r");
 
 	while(!feof(fp)) {
+		char line[BUFSIZ] = { 0, };
 		fgets(line, BUFSIZ-1, fp);
 		len = strlen(line);
 		for(i=0; len-i > 1;){
@@ -109,11 +160,11 @@ void unigramMaxFreq(char *fname, UnigramIndex *uniArr, int arrSize){
 					;
 				} else {
 					uniCnt[hIndex][lIndex]++;
+					// printf("%d: Found unigram %c%c\n", i, hIndex+0xA1,lIndex+0xA1);	
 				}
 				i += 2;
 			}
 		}
-		
 	}
 
 	for(int i = 0; i < arrSize; i++){
@@ -125,15 +176,16 @@ void unigramMaxFreq(char *fname, UnigramIndex *uniArr, int arrSize){
 	for(hIndex = 15; hIndex < 40; hIndex++){
 		for(lIndex = 0; lIndex < 94; lIndex++){
 			if(uniCnt[hIndex][lIndex] > minCnt){
-				swapIndex(uniArr, hIndex, lIndex, uniCnt[hIndex][lIndex], arrSize);
+				changeMinFreq(uniArr, hIndex, lIndex, uniCnt[hIndex][lIndex], arrSize);
 				minCnt = uniArr[0].cnt;
 			}
 		}
 	}
+	fclose(fp);
 }
 
 void printArr(UnigramIndex *arr, int size){
 	for(int i = 0; i < size; i++){
-		printf("|\t\t%d: %c%c  freq = %d\t\t|\n",i, arr[i].i+0xA1, arr[i].j+0xA1, arr[i].cnt);
+		printf("|\t\t%d: %c%c  freq = %d\t\t|\n",i, arr[size-1-i].i+0xA1, arr[size-1-i].j+0xA1, arr[size-1-i].cnt);
 	}
 }
