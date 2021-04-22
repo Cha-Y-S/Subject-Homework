36b84c320cb530e0b1d846af15cfedfcab70e765
Cha_Y_S <vaite714@gmail.com>
2021-04-22 21:58:40 +0900

    수정: sentence-similarity.py / 유사도 계산 및 출력 알고리즘 수정

--- 빅데이터 최신기술/과제 3/sentence-similarity.py
+++ 빅데이터 최신기술/과제 3/sentence-similarity.py
@@ -7,6 +7,7 @@
 # 20163162
 
 import sys
+import math
 
 def loadTrigramVector(file):
     dic = {}
@@ -18,20 +19,85 @@ def loadTrigramVector(file):
         temp = line[i].split()
         dic[temp[1]] = temp[0]
     
+    f.close()
+
     return dic
+
+def dot(vecA, vecB):
+    sum = 0
+    for i in range(0, len(va)):
+        for j in range(0, len(vb)):
+            if vecA[i][0] == vecB[j][0]:
+                sum += vecA[i][1] * vecB[j][1]
+    return sum
+
+def norm(v):
+    sum = 1
+    for i in ragne(0, len(v)):
+        sum += (v[i][1] * v[i][1])
+    return math.sqrt(sum)
+
+def getCosSimilarity(vecA, vecB):
+    return dot(vecA, vecB) / (norm(vecA) * norm(vecB))
+
+
+def vectorizeSentence(s, mat):
+    # result= [[id, freq], [id, freq], ...]
+
+
+def printSimilarSentence(s, sens):
+    print("----- Input Sentence")
+    print(" >> {sentence}".format(sentence=s))
+    print("----- Similar Sentence")
+    print(" >> 1. {sentence}\tsimilarity:{sim}"
+        .format(sentence = sens[1][0], sim = sens[1][1]))
+    print(" >> 2. {sentence}\tsimilarity:{sim}"
+        .format(sentence = sens[0][0], sim = sens[0][1]))
+
+def run(corpusFile, datFile):
+    vecMat = loadTrigramVector(datFile)
+
+    f = open(corpusFile, 'r')
+
+    line = f.readlines()
+
+    lineLimit = 10
+
+    for i in range(lineLimit):
+        simSentences = [["", 0], ["", 0]]
+        sen1 = line[i]
+        vec1 = vectorizeSentence(sen1, vecMat)
+        for j in range(len(line)):
+            if i == j:
+                continue
+            sen2 = line[j]
+            vec2 = vectorizeSentence(sen2, vecMat)
+            
+            temp = [sen2, getCosSimilarity(vec1, vec2)]
+
+            simSentences.sort(key=lambda x:x[1])
+
+            if simSentences[0][1] < temp[1]:
+                simSentence[0] = temp
+
+            simSentences.sort(key=lambda x:x[1])
+        
+        printSimilarSentence(sen1, simSentences)
     
+    return
+
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("========== Invalid Command ==========")
-        print("C> sentence-similarity.py corpus-EUC-KR.txt out.txt")
-        print("corpus must be encoded by EUC-KR")
-        print("out.txt is output of wordcount.exe(trigram.txt)")
+        print("========== Invalid Command ==========\n")
+        print("\tC> sentence-similarity.py corpus-EUC-KR.txt triVec.dat\n")
+        print("corpus must be encoded by EUC-KR\n")
+        print("triVec is preprocessed data by preprocessing.py\n")
         exit()
 
     corpus = sys.argv[1]
     triVec = sys.argv[2]
 
-    vecMat = loadTrigramVector(triVec)
-
-    print(vecMat)
+    run(corpus, triVec)
