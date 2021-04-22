4c408876218783365dc4cfc3d764b0b947d57250
Cha_Y_S <vaite714@gmail.com>
2021-04-22 20:42:23 +0900

    추가: sentence-similarity.py / 과제 메인 프로그램

---
+++ 빅데이터 최신기술/과제 3/sentence-similarity.py
@@ -0,0 +1,37 @@
+# Test for Korean sentences
+# input file : EUC-KR encoded 'corpus.txt' -- document
+#               && EUC-KR encoded 'out.txt' -- trigram's frequency
+#   input sentence: 1 line of input file
+#   output sentences: 2 sentence of input file that has most similarty
+# Written by Cha Yoonsung, vaite714@gmail.com
+# 20163162
+
+import sys
+
+def loadTrigramVector(file):
+    dic = {}
+
+    f = open(file, 'r')
+    line = f.readlines()
+
+    for i in range(len(line)):
+        temp = line[i].split()
+        dic[temp[1]] = temp[0]
+    
+    return dic
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("========== Invalid Command ==========")
+        print("C> sentence-similarity.py corpus-EUC-KR.txt out.txt")
+        print("corpus must be encoded by EUC-KR")
+        print("out.txt is output of wordcount.exe(trigram.txt)")
+        exit()
+
+    corpus = sys.argv[1]
+    triVec = sys.argv[2]
+
+    vecMat = loadTrigramVector(triVec)
+
+    print(vecMat)
