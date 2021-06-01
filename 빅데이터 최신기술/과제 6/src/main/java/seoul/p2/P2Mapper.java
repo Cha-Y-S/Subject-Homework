9a16ff949e76ba6d74183186c34d68ab32dc7877
Cha_Y_S <vaite714@gmail.com>
2021-06-01 23:02:31 +0900

    추가: 문제2 추가

---
+++ 빅데이터 최신기술/과제 6/src/main/java/seoul/p2/P2Mapper.java
@@ -0,0 +1,47 @@
+package seoul.p2;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class P2Mapper extends Mapper<Object, Text, Text, DoubleWritable> {
+	// arg1: Map Input Key type
+	// arg2: Map Input Value type
+	// arg3: Map Output Key type
+	// arg4: Map Output Value type
+
+	Text ok = new Text();
+	DoubleWritable ov = new DoubleWritable();
+
+	@Override
+	protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+		// 2017-01-01 00:00, 101, 1, 0.004, 0
+		// value.toString().split()
+		StringTokenizer st = new StringTokenizer(value.toString(), ",");
+
+		// set Key
+		st.nextToken();												// 2017-01-01 00:00
+		String stationCode = st.nextToken();	// 101
+		String itemCode = st.nextToken();			// 1
+
+		// itemCode == 8 -> PM10, itemCode == 9 -> PM2.5
+		if(Integer.parseInt(itemCode) != 8 && Integer.parseInt(itemCode) != 9) return;
+
+		// set Value
+		double itemValue = Double.parseDouble(st.nextToken());	// 0.004
+
+		String statusCode = st.nextToken();		// 0
+		if(Integer.parseInt(statusCode) != 0) return;	// statusCode == 0 -> normal data
+
+		ok.set(stationCode + "\t" + itemCode);
+
+		ov.set(itemValue);
+
+		// emit
+		context.write(ok, ov);
+	}
+}
