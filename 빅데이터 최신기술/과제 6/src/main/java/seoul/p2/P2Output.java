f54d28cc5e792311a52012690a0dceb52a85da63
Cha_Y_S <vaite714@gmail.com>
2021-06-02 22:43:01 +0900

    추가: 문제2 수정

--- 빅데이터 최신기술/과제 6/src/main/java/seoul/p2/P2Output.java
+++ 빅데이터 최신기술/과제 6/src/main/java/seoul/p2/P2Output.java
@@ -10,36 +10,23 @@ public class P2Output {
 		String inputPath = "src/data.csv_p2.out/part-r-00000";
 		BufferedReader br = new BufferedReader(new FileReader(inputPath));
 
-		String stationCode_PM10 = "";
-		String stationCode_PM2d5 = "";
-		Integer PM10 = Integer.MIN_VALUE;
-		Integer PM2d5 = Integer.MIN_VALUE;
+		String stationCode = "";
+		Integer count = Integer.MIN_VALUE;
 
 		while(true){
 			String line = br.readLine();
 			if(line == null) break;
 			StringTokenizer st = new StringTokenizer(line);
 			String tStationCode = st.nextToken();
-			String tItemName = st.nextToken();
 			Integer tValue = Integer.parseInt(st.nextToken());
 
-			switch(tItemName) {
-				case "PM10":
-					if (PM10 < tValue) {
-						PM10 = tValue;
-						stationCode_PM10 = tStationCode;
-					}
-					break;
-				case "PM2.5":
-					if (PM2d5 < tValue) {
-						PM2d5 = tValue;
-						stationCode_PM2d5 = tStationCode;
-					}
+			if(count < tValue) {
+				count = tValue;
+				stationCode = tStationCode;
 			}
 		}
 
-		System.out.println("PM10 기준 공기질 \"좋음\"이 " + PM10.toString() +"번으로 가장 많이 관측된 지역 코드는 " + stationCode_PM10 + "입니다.");
-		System.out.println("PM2.5 기준 공기질 \"좋음\"이 " + PM2d5.toString() +"번으로 가장 많이 관측된 지역 코드는 " + stationCode_PM2d5 + "입니다.");
+		System.out.println("공기질 \"좋음\"이 " + count + "번으로 가장 많이 출력된 지역 코드는 " + stationCode + "입니다.");
 
 
 	}
