076d264544a2a1b40e7b3de4ba7453e193d2aa6e
Cha_Y_S <vaite714@gmail.com>
2021-06-03 01:19:24 +0900

    수정: 문제2 / hadoop 실행 후 결과 바로 출력되도록 수정

--- 빅데이터 최신기술/과제 6/src/main/java/seoul/p2/Problem2.java
+++ 빅데이터 최신기술/과제 6/src/main/java/seoul/p2/Problem2.java
@@ -1,6 +1,8 @@
 package seoul.p2;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
@@ -12,10 +14,46 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Problem2 extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
 		ToolRunner.run(new Problem2(), args);
+
+		String inputPath = "data.csv_p2.out/part-r-0000";
+		int i = 0;
+		String line = "";
+		String stationCode = "";
+		Integer count = Integer.MIN_VALUE;
+
+		while(i < 3){
+			// Open HDFS's file
+			String nInputPath = inputPath + i;
+			Path inFile = new Path(nInputPath);
+			FileSystem fs = FileSystem.get(new Configuration());
+			BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(inFile)));
+
+			line = "";
+			while(true){
+				line = br.readLine();
+				if(line == null) break;
+				StringTokenizer st = new StringTokenizer(line);
+				String tStationCode = st.nextToken();
+				Integer tValue = Integer.parseInt(st.nextToken());
+
+				if(count < tValue) {
+					count = tValue;
+					stationCode = tStationCode;
+				}
+			}
+			i++;
+		}
+		System.out.println("공기질 \"좋음\"이 " + count + "번으로 가장 많이 출력된 지역 코드는 " + stationCode + "입니다.");
 	}
 
 	@Override
