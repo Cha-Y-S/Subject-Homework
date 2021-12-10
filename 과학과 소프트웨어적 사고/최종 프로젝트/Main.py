5b251966a035c1d34b4bc4b4ac172cfb8fd9f731
Cha_Y_S <vaite714@gmail.com>
2021-12-11 04:27:07 +0900

    Main Program 케이스별 메세지 출력 프로세스 구현

--- 과학과 소프트웨어적 사고/최종 프로젝트/Main.py
+++ 과학과 소프트웨어적 사고/최종 프로젝트/Main.py
@@ -1,23 +1,80 @@
 from Friend import Friend
 from Person import Person
+from Message import *
 
+# def print_add_menu():
 
-def print_main_menu():
+# def print_modify_menu():
 
+# def print_delete_menu():
 
-def print_add_menu():
+# def print_store_menu():
 
-
-def print_modify_menu():
-
-
-def print_delete_menu():
-
-
-def print_store_menu():
-
-
-def print_exit_program():
-
+# def print_exit_program():
 
 if __name__ == '__main__':
+    select_menu = 0
+    print_information()
+
+    while(True):
+        print_main_menu()
+        select_menu = input(" - 선택하려는 메뉴 번호를 입력해주세요: ")
+
+        try:
+            page_num = int(select_menu)
+            if page_num == 1:
+                # print("연락처 추가")
+                print_add_message()
+            elif page_num == 2:
+                # print("연락처 수정")
+                print_modify_message()
+            elif page_num == 3:
+                # print("연락처 삭제")
+                print_delete_message()
+            elif page_num == 4:
+                init_flag = False
+                while(True):
+                    print_init_message()
+                    init_input = input()
+                    print("")
+
+                    if init_input == "예":
+                        init_flag = True
+                        break
+                    elif init_input == "아니오":
+                        init_flag = False
+                        break
+                    else:
+                        print_tof_error_message()
+                        continue
+                if init_flag:
+                    print("== TODO: 연락처 초기화")
+
+            elif page_num == 5:
+                exit_flag = False
+
+                while(True):
+                    print_before_exit_message()
+                    exit_input = input()
+                    print("")
+
+                    if exit_input == "예":
+                        exit_flag = True
+                        break
+                    elif exit_input == "아니오":
+                        exit_flag = False
+                        break
+                    else:
+                        exit_flag = False
+                        print_tof_error_message()
+                        continue
+
+                if exit_flag:
+                    print(" ----- 프로그램을 종료합니다. -----".center(100))
+                    print("")
+                    break
+
+            else:
+                print_error()
+        except ValueError as e:
+            print_error()
---
+++ 과학과 소프트웨어적 사고/최종 프로젝트/Message.py
@@ -0,0 +1,78 @@
+def print_information():
+    print("="*50, "HELP", "="*50)
+    print("|", " ".rjust(15, " "), "- 연락처 관리 프로그램입니다.", "|".rjust(58))
+    print("|", "|".rjust(104))
+    print("|", " ".rjust(15, " "), "- 프로그램은 메뉴 번호 입력을 통해 진행됩니다.", "|".rjust(41))
+    print("|", "|".rjust(104))
+    print("|", " ".rjust(15, " "),
+          "- 메뉴 번호 입력 및 목적에 따라 입력을 진행하며 지인들의 연락처를 관리할 수 있습니다.", "|".rjust(2))
+    print("="*106, "\n")
+
+
+def print_main_menu():
+    top_bar = "*" * 14 + " 메인 메뉴 " + "*"*14
+    print(top_bar.center(100))
+    print("* 1. 연락처 추가".rjust(43), "*".rjust(22))
+    print("*".rjust(33), "*".rjust(37))
+    print("* 2. 연락처 수정".rjust(43), "*".rjust(22))
+    print("*".rjust(33), "*".rjust(37))
+    print("* 3. 연락처 삭제".rjust(43), "*".rjust(22))
+    print("*".rjust(33), "*".rjust(37))
+    print("* 4. 연락처 초기화".rjust(44), "*".rjust(20))
+    print("*".rjust(33), "*".rjust(37))
+    print("* 5. 프로그램 종료".rjust(44), "*".rjust(20))
+    bottom_bar = "*"*39
+    print(bottom_bar.center(104), "\n")
+
+
+def print_error():
+    print("")
+    print("*** 입력 오류 발생 ***".center(100))
+    print("")
+    print("잘못된 값을 입력하셨습니다.".center(96))
+    print("")
+    print("숫자 1 ~ 4 사이의 값을 입력해주세요.".center(100))
+    print("")
+
+
+def print_add_message():
+    print("")
+    print(" 연락처 추가 단계입니다".rjust(50, "="), "=".ljust(37, "="))
+    print("")
+
+
+def print_modify_message():
+    print("")
+    print(" 연락처 수정 단계입니다".rjust(50, "="), "=".ljust(37, "="))
+    print("")
+
+
+def print_delete_message():
+    print("")
+    print(" 연락처 삭제 단계입니다".rjust(50, "="), "=".ljust(37, "="))
+    print("")
+
+
+def print_init_message():
+    print("")
+    print("* 경고 *".center(100))
+    print("  -- 연락처 초기화를 선택하셨습니다. 모든 연락처가 삭제됩니다.")
+    print("")
+    print("  -- 정말로 연락처를 초기화하시겠습니까? (예 / 아니오) : ", end="")
+
+
+def print_before_exit_message():
+    print("")
+    print("  -- 프로그램의 종료를 선택하셨습니다.")
+    print("")
+    print("  -- 정말로 프로그램을 종료하시겠습니까? (예 / 아니오) : ", end="")
+
+
+def print_tof_error_message():
+    print("")
+    print("** 입력 오류 **".center(100))
+    print("")
+    print("  -- 예 / 아니오 중 하나를 입력해주세요.")
+    print("")
+
+    # def print_exit_program():
