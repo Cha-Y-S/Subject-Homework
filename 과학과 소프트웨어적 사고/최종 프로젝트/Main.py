d418c7d37484a53a243062e42b7edf73cec373bc
firemanCha <vaite714@gmail.com>
2021-12-10 23:25:39 +0900

    Main Menu 구성

---
+++ 과학과 소프트웨어적 사고/최종 프로젝트/Main.py
@@ -0,0 +1,23 @@
+from Friend import Friend
+from Person import Person
+
+
+def print_main_menu():
+
+
+def print_add_menu():
+
+
+def print_modify_menu():
+
+
+def print_delete_menu():
+
+
+def print_store_menu():
+
+
+def print_exit_program():
+
+
+if __name__ == '__main__':
